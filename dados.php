efe6c61330b44fe6fc261d665424c7d92437dc01
Ruan <55295644+ruannuness@users.noreply.github.com>
2020-05-21 13:51:04 -0300

    Sem Validação no PhP
    Formulário todo criado porem sem validação no backend.

---
+++ dados.php
@@ -0,0 +1,25 @@
+<?php
+
+$name = $_POST ["name"];
+$email = $_POST ["email"];
+$celular = $_POST ["celular"];
+$social = $_POST ["social"];
+$cnpj = $_POST ["cnpj"];
+$insestadual = $_POST["insestadual"];
+$insmunicipal = $_POST ["insmunicipal"];
+$alvasanitario = $_POST ["alvasanitario"];
+$telefoneemp = $_POST ["telefoneemp"];
+$celularemp = $_POST ["celularemp"];
+$emailemp = $_POST ["emailemp"];
+$cep = $_POST ["cep"];
+$logradouro = $_POST ["logradouro"];
+$numero = $_POST ["numero"];
+$bairro = $_POST ["bairro"];
+$cidade = $_POST ["cidade"];
+$uf = $_POST ["uf"];
+$latitude = $_POST ["latitude"];
+$longitude = $_POST ["longitude"];
+
+echo "Seu nome é: $name";
+
+?>
